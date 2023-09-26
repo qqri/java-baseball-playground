8d1224e65e0a137ca8b39aff4e33968cffca0959
qqri <cqo1154@gmail.com>
2023-09-26 22:40:20 +0900

    chore : readme 업데이트

--- src/test/java/study/BaseballGameSelfTest.java
+++ src/test/java/study/BaseballGameSelfTest.java
@@ -4,19 +4,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 class BaseballGameSelfTest {
-    private ArrayList<Integer> numbers;
+    private ArrayList<Integer> answer;
     @BeforeEach
     void setUp() {
-        numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
+        answer = new ArrayList<>();
+        answer.add(1);
+        answer.add(2);
+        answer.add(3);
     }
 
     @Test
@@ -27,6 +29,12 @@ class BaseballGameSelfTest {
         for(char c : input.toCharArray()) {
             inputList.add(c+"");
         }
+
+        input = inputList.stream()
+                        .map(String::valueOf)
+                                .collect(Collectors.toList(Collectors.joining()));
+
+
         assertThat(input).isEqualTo("123");
     }
 
